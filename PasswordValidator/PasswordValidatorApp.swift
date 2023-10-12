f4faabac2c161b5431d7fb23e6a4018ef37868d2
“Vladimir <vladimircezar@icloud.com>
2023-10-12 11:28:36 -0400

    Initial Commit

---
+++ PasswordValidator/PasswordValidatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasswordValidatorApp.swift
+//  PasswordValidator
+//
+//  Created by Vladimir Cezar on 2023-10-12.
+//
+
+import SwiftUI
+
+@main
+struct PasswordValidatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
